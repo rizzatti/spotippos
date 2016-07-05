f20e56b0f4da7079888dd89045868907e13c827e
José Otávio Rizzatti Ferreira <zehrizzatti@gmail.com>
2016-07-05 02:26:24 -0300

    Create properties table and mapping

---
+++ db/migrations/20160705052108_create_properties_table.rb
@@ -0,0 +1,15 @@
+Hanami::Model.migration do
+  change do
+    create_table :properties do
+      primary_key :id
+      column :title, String, null: false
+      column :description, String
+      column :price, Float
+      column :x, Integer, null: false
+      column :y, Integer, null: false
+      column :beds, Integer, null: false
+      column :baths, Integer, null: false
+      column :square_meters, Integer, null: false
+    end
+  end
+end
--- lib/config/mapping.rb
+++ lib/config/mapping.rb
@@ -8,3 +8,17 @@ collection :provinces do
   attribute :bottom, Integer
   attribute :left, Integer
 end
+
+collection :properties do
+  entity Property
+  repository PropertyRepository
+  attribute :id, Integer
+  attribute :title, String
+  attribute :description, String
+  attribute :price, Float
+  attribute :x, Integer
+  attribute :y, Integer
+  attribute :beds, Integer
+  attribute :baths, Integer
+  attribute :square_meters, Integer
+end
