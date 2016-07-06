98fdb4e5a23687fa3405709fc490d1d5700dca05
José Otávio Rizzatti Ferreira <zehrizzatti@gmail.com>
2016-07-06 03:03:24 -0300

    Add method to find properties within a box

--- lib/spotippos/repositories/property_repository.rb
+++ lib/spotippos/repositories/property_repository.rb
@@ -1,3 +1,10 @@
 class PropertyRepository
   include Hanami::Repository
+
+  def self.find_within_coordinates(top, right, bottom, left)
+    query do
+      where(['x <= ? and x >= ? and y >= ? and y <= ?',
+             top, bottom, left, right])
+    end.all
+  end
 end
--- spec/spotippos/repositories/property_repository_spec.rb
+++ spec/spotippos/repositories/property_repository_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe PropertyRepository do
-  # place your tests here
+  before do
+    PropertyRepository.clear
+    p1 = Property.new title: 'Property 1', price: 10, beds: 1, baths: 1,
+      square_meters: 50, x: 10, y: 10
+    p2 = Property.new title: 'Property 2', price: 10, beds: 1, baths: 1,
+      square_meters: 50, x: 20, y: 20
+    PropertyRepository.create p1
+    PropertyRepository.create p2
+  end
+
+  describe '#find_within_coordinates' do
+    it 'finds 2 properties accordingly' do
+      properties = PropertyRepository.find_within_coordinates(30, 30, 0, 0)
+      assert_equal(2, properties.size)
+    end
+
+    it 'finds 1 properties accordingly' do
+      properties = PropertyRepository.find_within_coordinates(15, 15, 0, 0)
+      assert_equal(1, properties.size)
+    end
+
+    it 'finds 0 properties accordingly' do
+      properties = PropertyRepository.find_within_coordinates(5, 5, 0, 0)
+      assert_equal(0, properties.size)
+    end
+  end
 end
