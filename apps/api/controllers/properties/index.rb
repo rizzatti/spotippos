2816165420c8fdab1e1e0656554fde45de4d07aa
José Otávio Rizzatti Ferreira <zehrizzatti@gmail.com>
2016-07-06 04:58:40 -0300

    Fix index action

--- apps/api/controllers/properties/index.rb
+++ apps/api/controllers/properties/index.rb
@@ -2,6 +2,8 @@ module Api::Controllers::Properties
   class Index
     include Api::Action
 
+    expose :properties, :provinces
+
     params do
       param :ax, presence: true, type: Integer, inclusion: 0..1400
       param :ay, presence: true, type: Integer, inclusion: 0..1000
@@ -11,6 +13,14 @@ module Api::Controllers::Properties
 
     def call(params)
       halt 400, '' unless params.valid?
+      top = params[:ay]
+      right = params[:bx]
+      bottom = params[:by]
+      left = params[:ax]
+      properties = PropertyRepository.find_within_coordinates(top, right,
+                                                              bottom, left)
+      @properties = properties.map {|e| PropertyDecorator.new e }
+      @provinces = ProvinceRepository.all
     end
   end
 end
